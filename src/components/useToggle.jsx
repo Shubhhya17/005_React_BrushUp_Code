c8c482b59cfd22b65b94c42e0d52ef95cba79cf5
shubham-pawar01 <shubhamdev@mrchms.com>
2025-09-12 21:49:03 +0530

    useState

--- src/components/useToggle.jsx
+++ src/components/useToggle.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const useToggle = () => {
-  return (
-    <div>useToggle</div>
+const useToggle = (defaultVal) => {
+    const [value,setValue] = useState();
+
+    function toggleValue(val){
+        if(typeof val !='boolean'){
+            setValue(!value)
+        }else{
+            setValue(val)
+        }
+    }
+  return ( 
+
+<>
+<button>Toggle Heading</button>
+<button>Hide Heading</button>
+<button>Show Heading</button>
+</>
+
   )
 }
 
